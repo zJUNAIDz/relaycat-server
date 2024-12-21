713ebfda3b807b7d55db1a7fcd7e5137cbba8774
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-12-21 20:54:20 +0530

    add jwt and cors middleware

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import "dotenv/config";
 import { Hono } from "hono";
 import { serveStatic } from "hono/bun";
 import { cors } from "hono/cors";
+import { jwt, JwtVariables } from "hono/jwt";
 import s3Routes from "./routes/s3.route";
 import ServersRoutes from "./routes/servers.route";
 
-const app = new Hono();
+type Variables = JwtVariables;
+const app = new Hono<{ Variables: Variables }>();
+
 const clientUrl = process.env.CLIENT_URL!;
 if (!clientUrl) throw new Error("CLIENT_URL is not defined");
-
 app.use(
   "*",
   cors({
@@ -16,13 +18,14 @@ app.use(
     credentials: true,
   })
 );
+app.use(jwt({ secret: process.env.JWT_SECRET! }));
 app.use("/static/*", serveStatic({ root: "./" }));
 
 app.route("/s3", s3Routes);
 app.route("/servers", ServersRoutes);
 
 app.get("/", (c) => {
-  return c.html("<h1> This is an Internal API.</h1>");
+  return c.html(`<h1> This is an Internal API.</h1>`);
 });
 
 export default {
