784e6a263473d63dcb9d206b37f5b5f7200f7535
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-12-24 22:15:30 +0530

    add token parser

---
+++ src/utils/token.ts
@@ -0,0 +1,11 @@
+import { verify } from "hono/jwt";
+import { getEnv } from "./env";
+
+export const parseToken = async (
+  token: string,
+  secretKey = getEnv("JWT_SECRET")
+) => {
+  const decodedPayload = await verify(token, secretKey);
+  console.log(decodedPayload);
+  return decodedPayload;
+};
