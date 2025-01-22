61bef3ed4ad78d4b3aba86d37af3d8045287c77b
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-01-22 11:34:27 +0530

    add `members` route implement `/members/changeRole` and `/members/kick` endpoints

---
+++ src/routes/members.route.ts
@@ -0,0 +1,95 @@
+import { Hono } from "hono";
+import { db } from "../lib/db";
+
+const membersRoutes = new Hono();
+
+membersRoutes.patch("/changeRole", async (c) => {
+  try {
+    const { serverId, memberId } = c.req.query();
+    const { role } = await c.req.json();
+    if (!serverId) {
+      return c.json({ error: "Server ID is required" }, 400);
+    }
+    if (!memberId) {
+      return c.json({ error: "Member ID is required" }, 400);
+    }
+    if (!role) {
+      return c.json({ error: "Role is required" }, 400);
+    }
+    console.table({ serverId, memberId, role });
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        userId: c.get("jwtPayload").id,
+      },
+      data: {
+        members: {
+          update: {
+            where: {
+              id: memberId,
+              userId: {
+                not: c.get("jwtPayload").id,
+              },
+            },
+            data: {
+              role,
+            },
+          },
+        },
+      },
+      include: {
+        members: {
+          include: {
+            user: true,
+          },
+          orderBy: {
+            role: "asc",
+          },
+        },
+      },
+    });
+    return c.json({ server });
+  } catch (err) {
+    console.error("[MEMBERS_ID_PATCH] ", err);
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
+
+membersRoutes.delete("/kick", async (c) => {
+  try {
+    const memberId = c.req.query("memberId");
+    if (!memberId) {
+      return c.json({ error: "Member ID is required" }, 400);
+    }
+    const serverId = c.req.query("serverId");
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        userId: c.get("jwtPayload").id,
+      },
+      data: {
+        members: {
+          deleteMany: {
+            id: memberId,
+            userId: {
+              not: c.get("jwtPayload").id,
+            },
+          },
+        },
+      },
+      include: {
+        members: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+    return c.json({ server });
+  } catch (err) {
+    console.error("[MEMBERS_ID_PATCH] ", err);
+    return c.json({ error: "Internal Server Error" }, 500);
+  }
+});
+
+export default membersRoutes;
