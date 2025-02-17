abde35d0871eba0c911dd84caad09608d2e97393
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-02-18 00:03:51 +0530

    add type definitions for Server responses

---
+++ types/index.ts
@@ -0,0 +1,18 @@
+import { Channel, Member, Server, User } from "@prisma/client";
+
+export type ServerWithMembersAndUser = Server & {
+  members: (Member & { user: User })[];
+};
+
+export type ServerWithMembersUserAndChannels = Server & {
+  members: (Member & { user: User })[];
+  channels: Channel[];
+}
+
+export type ServerWithMembersOnly = Server & {
+  members: Member[];
+};
+
+export type ServerWithChannels = Server & {
+  channels: Channel[];
+};
