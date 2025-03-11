622c179261177c248628485b4a60186e8bd5e016
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-03-12 01:53:03 +0530

    feat✨: implement service method to create message and emit socket event

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -1,7 +1,10 @@
-import { Channel, Member, Message, User } from "@prisma/client";
+import { Channel, Member, Message } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { db } from "../lib/db";
+import { socketManager } from "../lib/socket-manager";
 
 class MessageService {
+  private MESSAGE_BATCH = 10;
   async createMessage(content: Message["content"], channelId: Channel["id"], memberId: Member["id"]) {
     try {
       const message = await db.message.create({
@@ -11,17 +14,23 @@ class MessageService {
           memberId,
         },
         include: {
-          member: true,
+          member: {
+            include: {
+              user: true
+            }
+          },
         },
       });
       if (!message) {
         return { error: "Message not created" }
       }
+      socketManager.io.emit(`chat:${channelId}:messages`, message)
       return { message }
     } catch (error) {
       console.error("[createMessage] ", error)
       return { error }
     }
   }
+  
 }
 export const messageService = new MessageService();
