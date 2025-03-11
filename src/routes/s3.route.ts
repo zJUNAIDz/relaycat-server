8a2491d9e64bf440b9a8e4154f7778f9e15ebb5d
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-03-12 02:04:58 +0530

    feat✨: Implement route to GET `/s3/signed-url`. it supports images and PDFs

--- src/routes/s3.route.ts
+++ src/routes/s3.route.ts
@@ -14,10 +14,34 @@ s3Routes.get("/uploadNewImage", async (c: Context) => {
     return c.json({ error: "fileType is required" }, 400);
 
   const fileExtension = fileType.split("/")[1];
-  if (!["png", "jpg", "jpeg", "webp"].includes(fileExtension)) {
+  if (!["png", "jpg", "jpeg", "webp", "pdf", "gif"].includes(fileExtension)) {
     return c.json({ error: "fileType is not supported" }, 400);
   }
   const key = `${serverName}-${crypto.randomUUID()}.${fileExtension}`;
+  const bucketName = getEnv("AWS_S3_BUCKET_NAME");
+
+
+  const { data, error } = await s3Service.getUploadNewImageUrl(key, fileType);
+  if (error) return c.json({ error }, 500);
+  const signedUrl = data?.signedUrl;
+  if (!signedUrl) return c.json({ error: "Error getting signed url" }, 500);
+  return c.json({ signedUrl, key, bucketName });
+});
+
+s3Routes.get("/signed-url", async (c) => {
+  const { fileName, fileType } = c.req.query();
+  if (!fileName || fileName.trim() === "") {
+    return c.json({ error: "serverName is required" }, 400);
+  }
+
+  if (!fileType || fileType.trim() === "")
+    return c.json({ error: "fileType is required" }, 400);
+
+  const fileExtension = fileType.split("/")[1];
+  if (!["png", "jpg", "jpeg", "webp", "pdf", "gif"].includes(fileExtension)) {
+    return c.json({ error: "fileType is not supported" }, 400);
+  }
+  const key = `${fileName}-${crypto.randomUUID()}.${fileExtension}`;
   const bucketName = getEnv("AWS_S3_BUCKET_NAME");
 
 
